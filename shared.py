200f6cea33dcb71b84ef1f2ba52bb74793903889
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2020-10-19 13:20:19 -0700

    Detect negative scores
    Allows recovering from an over-deduction.
    Doesn't protect against entering invalid scores.

--- shared.py
+++ shared.py
@@ -60,9 +60,9 @@ MAX_SCORES = [10, 20, 20, 40, 10]
 SUMMARY_COMMENT = "TOTAL CONTENT: 100\nTOTAL DEDUCTIONS: 0\nGRADE: 100/100"
 # We match on "\r" because Microsoft Word implicitly converts "\n" to "\r" on insertion
 SUMMARY_COMMENT_PATTERN = re.compile(
-    r"(?P<content>TOTAL CONTENT: )\d+\r"
-    r"(?P<deductions>TOTAL DEDUCTIONS: )\d+\r"
-    r"(?P<grade>GRADE: )\d+"
+    r"(?P<content>TOTAL CONTENT: )-?\d+\r"
+    r"(?P<deductions>TOTAL DEDUCTIONS: )-?\d+\r"
+    r"(?P<grade>GRADE: )-?\d+"
 )
 
 # The header comments to be inserted
@@ -72,7 +72,7 @@ HEADER_COMMENTS = [
 ]
 # The pattern for finding header comments as well as substituting scores within them
 HEADER_COMMENT_PATTERNS = [
-    re.compile(fr"(?P<header>{header.upper()}: )\d+") for header in FULL_HEADERS
+    re.compile(fr"(?P<header>{header.upper()}: )-?\d+") for header in FULL_HEADERS
 ]
 
 
@@ -81,10 +81,10 @@ HEADER_COMMENT_PATTERNS = [
 # Patterns here use named groups to be reused during scoring operations.
 
 # Matches comments with a number at the very start of the comment
-CONTENT_POINTS_LOST_PATTERN = re.compile(r"(?P<value>\d+)")
+CONTENT_POINTS_LOST_PATTERN = re.compile(r"(?P<value>-?\d+)")
 
 # Matches comments with "D", then a number, at the very start of the comment
-DEDUCTION_PATTERN = re.compile(r"D(?P<value>\d+)")
+DEDUCTION_PATTERN = re.compile(r"D(?P<value>-?\d+)")
 
 
 # * -------------------------------------------------------------------------------- * #
