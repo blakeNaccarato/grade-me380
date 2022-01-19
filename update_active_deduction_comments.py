decd83badf50a7f53af4ed512a88dc60e1343823
blakeNaccarato <blake.naccarato@gmail.com>
2022-01-19 12:52:20 -0800

    Actually, let's process deductions in update_grade

--- update_active_deduction_comments.py
+++
@@ -1,4 +0,0 @@
-from dynaconf import Dynaconf
-
-deductions = Dynaconf(settings_files=["deductions.yaml"]).deductions
-print([deduction.text for deduction in deductions if deduction.code == "G3"][0])
