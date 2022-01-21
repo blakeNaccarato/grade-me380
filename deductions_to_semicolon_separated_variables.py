3bcece339e79cc6d112c96a08f677c9bebf500aa
blakeNaccarato <20692450+blakeNaccarato@users.noreply.github.com>
2022-01-20 18:19:14 -0800

    Update nondescript variable name

--- deductions_to_semicolon_separated_variables.py
+++ deductions_to_semicolon_separated_variables.py
@@ -3,9 +3,9 @@ import csv
 import yaml
 
 with open("deductions_common.yaml") as file:
-    test = yaml.safe_load(file)
+    all_deductions = yaml.safe_load(file)
 
 with open("deductions.csv", "w", newline="") as file:
     writer = csv.writer(file, delimiter=";")
-    for key, value in test.items():
+    for key, value in all_deductions.items():
         writer.writerow([key, value])
